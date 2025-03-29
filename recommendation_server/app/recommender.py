aaa345de258b31881f8b0c84889a41d0e88e9122
Ed-Harutyunyan <edgar.harutiunyan@icloud.com>
2025-03-29 20:09:47 +0400

    Infisical Upgrade

--- recommendation_server/app/recommender.py
+++ recommendation_server/app/recommender.py
@@ -7,21 +7,35 @@ import os
 import openai
 from dotenv import load_dotenv
 
-# client = InfisicalSDKClient(host="https://app.infisical.com")
-#
-# client.auth.universal_auth.login(
-#     client_id=os.getenv("INFISICAL_CLIENT_ID"),
-#     client_secret=os.getenv("INFISICAL_CLIENT_SECRET")
-# )
-#
-# secret = client.secrets.get_secret_by_name(
-#     secret_name="OPENAI_API",
-#     project_id=os.getenv("INFISICAL_PROJECT_ID"),
-#     environment_slug="dev",
-#     secret_path="/"
-# )
-
 load_dotenv("../../.env")
+
+client = InfisicalSDKClient(host="https://app.infisical.com")
+
+client.auth.universal_auth.login(
+    client_id=os.getenv("INFISICAL_CLIENT_ID"),
+    client_secret=os.getenv("INFISICAL_CLIENT_SECRET")
+)
+
+openai_key = client.secrets.get_secret_by_name(
+    secret_name="OPENAI_API",
+    project_id=os.getenv("INFISICAL_PROJECT_ID"),
+    environment_slug="dev",
+    secret_path="/"
+)
+
+qdrant_key = client.secrets.get_secret_by_name(
+    secret_name="QDRANT_API_KEY",
+    project_id=os.getenv("INFISICAL_PROJECT_ID"),
+    environment_slug="dev",
+    secret_path="/"
+)
+
+qdrant_url = client.secrets.get_secret_by_name(
+    secret_name="QDRANT_URL",
+    project_id=os.getenv("INFISICAL_PROJECT_ID"),
+    environment_slug="dev",
+    secret_path="/"
+)
 
 collection_name = "courses_collection"
 
